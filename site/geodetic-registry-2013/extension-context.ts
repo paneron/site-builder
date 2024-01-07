80603c19850eb82e38302024c0d26ff879067fc0
Anton <anton@strogonoff.name>
2024-01-07 17:20:11 +0800

    refactor(template): organize & unbreak date format
    Now allows “lax” non-ISO dates.

--- site/geodetic-registry-2013/extension-context.ts
+++ site/geodetic-registry-2013/extension-context.ts
@@ -10,31 +10,12 @@ import type { Hooks, DatasetContext } from '@riboseinc/paneron-extension-kit/typ
 
 type Dataset = Record<string, Record<string, unknown>>;
 
-type Predicate = (
-  /**
-   * Also known as “key”
-   * (a custom keyer can make it be something other than object path).
-   */
-  objPath: string,
-  /**
-   * Object data (value).
-   */
-  obj: Record<string, unknown>,
-) => boolean
 
-/** Allows to index objects by something other than their paths. */
-type Keyer = (obj: Record<string, unknown>) => string
-
-type MapFunc = (
-  /** Object path. */
-  key: string,
-  /** Object data. */
-  value: unknown,
-  /** Called for matches. */
-  emit: (val: unknown) => void,
-) => void
-
-type ReduceFunc = (accumulator: unknown, current: unknown) => void
+const GLOBAL_SETTINGS = {
+  mainNavbarPosition: 'top',
+  sidebarPosition: 'left',
+  defaultTheme: 'light',
+} as const;
 
 
 export function getExtensionContext(
@@ -43,9 +24,16 @@ export function getExtensionContext(
 
   const ctx: DatasetContext = {
     title: "Paneron dataset",
-    useGlobalSettings,
     logger: console,
+
     openExternalLink: async ({ uri }) => { window.open(uri, '_blank') },
+
+    useGlobalSettings: function () {
+      return {
+        ...VALUE_HOOK_STUB,
+        value: { settings: GLOBAL_SETTINGS, },
+      } as const;
+    },
 
     usePersistentDatasetStateReducer: (...opts) => {
       const effectiveOpts:
@@ -67,19 +55,12 @@ export function getExtensionContext(
       return usePersistentStateReducer(noOp, alwaysLoadInitialState, ...effectiveOpts);
     },
 
-    useObjectData: function ({ objectPaths, nounLabel }) {
-      return {
-        ...VALUE_HOOK_STUB,
-        value: {
-          data: getObjectData(data, objectPaths) as any,
-        },
-      };
-    },
+    useObjectData: makeValueHook(function ({ objectPaths, nounLabel }) {
+      return { data: getObjectData(data, objectPaths) };
+    }),
 
     getObjectData: async function ({ objectPaths }) {
-      return {
-        data: getObjectData(data, objectPaths) as any,
-      }
+      return { data: getObjectData(data, objectPaths) };
     },
 
     useRemoteUsername: function () {
@@ -87,72 +68,48 @@ export function getExtensionContext(
       return { ...VALUE_HOOK_STUB, value: {} };
     },
 
-    useSettings: function () {
-      // Stub
-      return { ...VALUE_HOOK_STUB, value: { settings: {} } };
-    },
+    useSettings: makeValueHook(function () {
+      // XXX: Implement useSettings with some local storage
+      return { settings: {} };
+    }),
 
-    useMapReducedData: function ({ chains }) {
-      return {
-        ...VALUE_HOOK_STUB,
-        value: mapReduceChains(data, chains) as any,
-      };
-    },
+    useMapReducedData: makeValueHook(function ({ chains }) {
+      return mapReduceChains(data, chains) as any;
+    }),
 
     getMapReducedData: async function  ({ chains }) {
       return mapReduceChains(data, chains) as any;
     },
 
-    useFilteredIndex: function ({ queryExpression, keyExpression }) {
-      // const predicate = new Function('objPath', 'obj', queryExpression);
-      // const keyer = keyExpression
-      //   ? new Function('obj', keyExpression)
-      //   : undefined;
-      return {
-        ...VALUE_HOOK_STUB,
-        value: { indexID: getIndexID(queryExpression, keyExpression) },
-      };
-    },
+    useFilteredIndex: makeValueHook(function ({ queryExpression, keyExpression }) {
+      return { indexID: getSimpleQueryKey(queryExpression, keyExpression) };
+    }),
 
-    useIndexDescription: function ({ indexID }) {
+    useIndexDescription: makeValueHook(function ({ indexID }) {
       return {
-        ...VALUE_HOOK_STUB,
-        value: {
-          status: {
-            objectCount: indexID
-              ? matchObjectsWithCache(data, indexID).length
-              : 0,
-          },
+        status: {
+          objectCount: indexID
+            ? listPathsCached(data, indexID).length
+            : 0,
         },
       };
-    },
+    }),
 
-    useObjectPathFromFilteredIndex: function ({ indexID, position }) {
-      const objPaths = matchObjectsWithCache(data, indexID)
-      const objectPath = objPaths[position] ?? '';
-      return {
-        ...VALUE_HOOK_STUB,
-        value: { objectPath },
-      };
-    },
+    useObjectPathFromFilteredIndex: makeValueHook(function ({ indexID, position }) {
+      return { objectPath: listPathsCached(data, indexID)[position] ?? '' };
+    }),
 
     getObjectPathFromFilteredIndex: async function ({ indexID, position }) {
-      const objPaths = matchObjectsWithCache(data, indexID)
-      return { objectPath: objPaths[position] ?? '' };
+      return { objectPath: listPathsCached(data, indexID)[position] ?? '' };
     },
 
-    useFilteredIndexPosition: function ({ indexID, objectPath }) {
-      const objPaths = matchObjectsWithCache(data, indexID);
-      const pos = objPaths.indexOf(objectPath);
-      return {
-        ...VALUE_HOOK_STUB,
-        value: { position: pos >= 0 ? pos : null },
-      };
-    },
+    useFilteredIndexPosition: makeValueHook(function ({ indexID, objectPath }) {
+      const pos = listPathsCached(data, indexID).indexOf(objectPath);
+      return { position: pos >= 0 ? pos : null };
+    }),
 
     getFilteredIndexPosition: async function ({ indexID, objectPath }) {
-      const objPaths = matchObjectsWithCache(data, indexID);
-      const pos = objPaths.indexOf(objectPath);
+      const pos = listPathsCached(data, indexID).indexOf(objectPath);
       return { position: pos >= 0 ? pos : null };
     },
   };
@@ -161,113 +118,32 @@ export function getExtensionContext(
 }
 
 
-const VALUE_HOOK_STUB = {
-  errors: [] as string[],
-  isUpdating: false,
-  _reqCounter: 0,
-  refresh: () => {},
-} as const;
+// Simple queries
+// ==============
 
-
-function useGlobalSettings() {
-  return {
-    value: {
-      settings: { mainNavbarPosition: 'top', sidebarPosition: 'left', defaultTheme: 'light' },
-    },
-    ...VALUE_HOOK_STUB,
-  } as const;
-}
-
-
-function noOp() {};
-
-
-
-// INDEX UTILS
-// ===========
-
-
-const INDICES: Record<string, { objPaths: string[] }> = {};
-
-
-function getObjectData(data: Dataset, objectPaths: string[]) {
-  const d= objectPaths.
-    map(objPath => ({ [objPath]: parseData(data[objPath]) ?? null })).
-    reduce((prev, curr) => ({ ...prev, ...curr }), {});
-  return d;
-}
-
-const parseDate = S.parseSync(S.Date);
-
-function parseData(val: unknown, _seen?: WeakSet<any>) {
-  const seen = _seen ?? new WeakSet();
-
-  if (seen.has(val)) {
-    return val;
-  }
-
-  if (val && isObject(val)) {
-    seen.add(val);
-    return Object.entries(val as Record<string, unknown>).
-      map(([k, v]): Record<string, unknown> => ({ [k]: parseData(v, seen) })).
-      reduce((prev, curr) => ({ ...prev, ...curr }), {});
-  } else if (val && Array.isArray(val)) {
-    return val.map((val): unknown => parseData(val, seen));
-  } else if (typeof val === 'string') {
-    try {
-      const date = parseDate(val);
-      if (date.toISOString() === val) {
-        return date;
-      } else {
-        return val;
-      }
-    } catch (e) {
-      return val;
-    }
-  } else {
-    return val;
-  }
-}
-
-function mapReduceChains(data: Dataset, chains: Hooks.Data.MapReduceChains) {
-  return Object.entries(chains).
-  map(([cID, c]) => {
-
-    const mapped: Record<string, unknown> = {}
-    function handleEmit(objPath: string, val: unknown) {
-      mapped[objPath] = val;
-    }
-    const mapFunc = getMapFunc(c.mapFunc);
-    const objPaths = c.predicateFunc
-      ? matchObjectsWithCache(data, getIndexID(c.predicateFunc))
-      : Object.keys(data);
-    for (const objPath of objPaths) {
-      mapFunc(
-        objPath,
-        data[objPath],
-        (val) => handleEmit(objPath, val))
-    }
-
-    let final;
-    if (c.reduceFunc) {
-      const reduceFunc = getReduceFunc(c.reduceFunc);
-      final = Object.values(mapped).reduce(reduceFunc);
-    } else {
-      final = Object.values(mapped) || [];
-    }
-
-    return { [cID]: final };
-  }).
-  reduce((prev, curr) => ({ ...prev, ...curr }), {})
-}
-
-function matchObjects(
+/** Returns a list of paths matching a serialized simple query. */
+function listPathsCached(
   d: Dataset,
-  predicateString: string,
-  keyerString?: string,
+  query: string,
 ): string[] {
-  const predicate = getPredicate(predicateString);
-  const keyer = keyerString ? getKeyer(keyerString) : undefined;
+  if (!CACHE[query]) {
+    const runnableQuery = getRunnableSimpleQuery(query);
+    CACHE[query] = {
+      objPaths: listPathsMatchingSimpleQuery(d, runnableQuery)
+    };
+  }
+  return CACHE[query]!.objPaths;
+}
+const CACHE: Record<string, { objPaths: string[] }> = {};
+
+
+/** Given a query, returns an ordered list of object paths. */
+function listPathsMatchingSimpleQuery(
+  d: Dataset,
+  query: RunnableSimpleQuery,
+): string[] {
+
+  const { predicateFunc: predicate, keyerFunc: keyer } = query;
 
   const keyed: Record<string, string> = {};
   if (keyer) {
@@ -296,26 +172,93 @@ function matchObjects(
   return objPaths;
 }
 
-function matchObjectsWithCache(
-  d: Dataset,
-  query: string,
-): string[] {
-  if (!INDICES[query]) {
-    const { predicateString, keyerString } = unpackQuery(query);
-    INDICES[query] = { objPaths: matchObjects(d, predicateString, keyerString) };
-  }
-  return INDICES[query]!.objPaths;
+const SimpleQuery = S.struct({
+  predicateString: S.string,
+  keyerString: S.optional(S.union(S.string, S.undefined)),
+});
+
+interface RunnableSimpleQuery {
+  predicateFunc: Predicate;
+  keyerFunc?: Keyer;
 }
 
+function getSimpleQueryKey(predicateString: string, keyerString?: string): string {
+  const query = S.parseSync(SimpleQuery)({ predicateString, keyerString });
+  return JSON.stringify(query);
+}
 
-function getPredicate(predicateString: string): Predicate {
+function getRunnableSimpleQuery(queryKey: string): RunnableSimpleQuery {
+  const query = S.parseSync(SimpleQuery)(JSON.parse(queryKey));
+
+  const keyerFunc = query.keyerString
+    ? deserializeKeyer(query.keyerString)
+    : undefined;
+
+  return {
+    predicateFunc: deserializePredicate(query.predicateString),
+    ...(keyerFunc ? { keyerFunc } : {}),
+  };
+}
+
+function deserializePredicate(predicateString: string): Predicate {
   // XXX: simulated validation
   return new Function('objPath', 'obj', predicateString) as Predicate;
 }
 
-function getKeyer(keyExpression: string): Keyer {
+function deserializeKeyer(keyExpression: string): Keyer {
   // XXX: simulated validation
   return new Function('obj', keyExpression) as Keyer;
+}
+
+type Predicate = (
+  /**
+   * Also known as “key”
+   * (a custom keyer can make it be something other than object path).
+   */
+  objPath: string,
+  /**
+   * Object data (value).
+   */
+  obj: Record<string, unknown>,
+) => boolean
+
+/** Allows to index objects by something other than their paths. */
+type Keyer = (obj: Record<string, unknown>) => string
+
+
+// Map-reduce queries
+// ==================
+
+function mapReduceChains(data: Dataset, chains: Hooks.Data.MapReduceChains) {
+  return Object.entries(chains).
+  map(([cID, c]) => {
+
+    const mapped: Record<string, unknown> = {}
+    function handleEmit(objPath: string, val: unknown) {
+      mapped[objPath] = val;
+    }
+    const mapFunc = getMapFunc(c.mapFunc);
+    const objPaths = c.predicateFunc
+      ? listPathsCached(data, getSimpleQueryKey(c.predicateFunc))
+      : Object.keys(data);
+    for (const objPath of objPaths) {
+      mapFunc(
+        objPath,
+        parseData(data[objPath]),
+        (val) => handleEmit(objPath, val))
+    }
+
+    let final;
+    if (c.reduceFunc) {
+      const reduceFunc = getReduceFunc(c.reduceFunc);
+      final = Object.values(mapped).reduce(reduceFunc);
+    } else {
+      final = Object.values(mapped) || [];
+    }
+
+    return { [cID]: final };
+  }).
+  reduce((prev, curr) => ({ ...prev, ...curr }), {});
 }
 
 function getMapFunc(mapFunc: string): MapFunc {
@@ -328,15 +271,97 @@ function getReduceFunc(reduceFunc: string): ReduceFunc {
   return new Function('accumulator', 'value', reduceFunc) as ReduceFunc;
 }
 
-function getIndexID(predicateString: string, keyerString?: string): string {
-  return JSON.stringify({ predicateString, keyerString });
+type MapFunc = (
+  /** Object path. */
+  key: string,
+  /** Object data. */
+  value: unknown,
+  /** Called for matches. */
+  emit: (val: unknown) => void,
+) => void
+
+type ReduceFunc = (accumulator: unknown, current: unknown) => void
+
+
+// Fetching specific object data
+// =============================
+
+function getObjectData(data: Dataset, objectPaths: string[]):
+Record<string, Record<string, unknown> | null> {
+  const d= objectPaths.
+    map(objPath => ({
+      [objPath]:
+        (parseData(data[objPath]) as Record<string, unknown>)
+        ?? null,
+    })).
+    reduce((prev, curr) => ({ ...prev, ...curr }), {});
+  return d;
 }
 
-const IndexQuery = S.struct({
-  predicateString: S.string,
-  keyerString: S.union(S.string, S.undefined),
-});
+/**
+ * Recursively processes deserialized object data, additionally deserializing:
+ *
+ * - String ISO timestamps into Date instances.
+ */
+function parseData(val: unknown, _seen?: WeakSet<any>) {
+  const seen = _seen ?? new WeakSet();
 
-function unpackQuery(indexID: string): S.Schema.To<typeof IndexQuery> {
-  return JSON.parse(indexID);
+  if (seen.has(val)) {
+    return val;
+  }
+
+  if (val && isObject(val)) {
+    seen.add(val);
+    return Object.entries(val as Record<string, unknown>).
+      map(([k, v]): Record<string, unknown> => ({ [k]: parseData(v, seen) })).
+      reduce((prev, curr) => ({ ...prev, ...curr }), {});
+  } else if (val && Array.isArray(val)) {
+    // XXX: If array can be added to “seen”, it should.
+    return val.map((val): unknown => parseData(val, seen));
+  } else if (typeof val === 'string') {
+    return maybeDate(val);
+  } else {
+    return val;
+  }
 }
+
+/** If string is a valid ISO date, returns Date; otherwise the string itself. */
+function maybeDate(val: string): Date | string {
+  try {
+    const date = _parseDate(val);
+    const fullISO = date.toISOString();
+    if (fullISO === val || fullISO.split('T')[0] === val) {
+      return date;
+    } else {
+      return val;
+    }
+  } catch (e) {
+    return val;
+  }
+}
+const _parseDate = S.parseSync(S.Date);
+
+
+// Extension kit helpers
+// =====================
+
+function makeValueHook<I extends unknown[], O>(valueGetter: (...args: I) => O):
+(...args: I) => (typeof VALUE_HOOK_STUB) & { value: O } {
+  return function (...args) {
+    const value = valueGetter(...args)
+    return { ...VALUE_HOOK_STUB, value };
+  }
+}
+
+const VALUE_HOOK_STUB = {
+  errors: [] as string[],
+  isUpdating: false,
+  _reqCounter: 0,
+  refresh: () => {},
+} as const;
+
+
+// Other utils
+// ===========
+
+function noOp() {};
