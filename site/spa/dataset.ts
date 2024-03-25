867f033c88aae38563aba9df2e8dc264f577bad9
Anton <anton@strogonoff.name>
2024-03-25 16:45:27 +0800

    chore(spa): add future note about forUsername

--- site/spa/dataset.ts
+++ site/spa/dataset.ts
@@ -3,9 +3,13 @@ import * as S from '@effect/schema/Schema';
 
 export const DatasetSchema = S.record(S.string, S.record(S.string, S.unknown));
 //export const ManifestSchema = S.record(S.string, S.string);
+
+
 export const ManifestSchema = S.struct({
-  forUsername: S.optional(S.union(S.string.pipe(S.nonEmpty()), S.undefined)),
   dataVersion: S.optional(S.union(S.string.pipe(S.nonEmpty()), S.undefined)),
+
+  // TODO: Should go away at some point.
+  forUsername: S.optional(S.union(S.string.pipe(S.nonEmpty()), S.undefined)),
 });
 
 export type Dataset = Record<string, Record<string, unknown>>;
