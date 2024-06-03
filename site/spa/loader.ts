ce989ca21f5bb1d07ec12dbeca9741d2635578a8
Anton <anton@strogonoff.name>
2024-06-03 22:02:01 +0800

    feat(spa): fine-grained loading status

--- site/spa/loader.ts
+++ site/spa/loader.ts
@@ -16,27 +16,33 @@ const byteFormatter = Intl.NumberFormat(navigator.language, {
 
 const LOAD_STATUS: Record<string, { done: number, total: number }> = {};
 
-function loadScript<SRCS extends readonly string[]>(
-  srcs: SRCS,
+function loadAsset<Src extends string>(
+  srcs: readonly Src[],
   onProgress: (done: number[], total: number[]) => void,
-  onError: (msg?: string, resp?: XMLHttpRequest["response"]) => void,
-  onDone: (src: string) => void,
+  onError: (assetSrc: Src, msg?: string, resp?: XMLHttpRequest["response"]) => void,
+  onDone: (assetSrc: Src) => void,
 ) {
-  const xhrs: { [src in SRCS[number]]: XMLHttpRequest } = srcs.
-    map((scriptSrc: SRCS[number]) => {
-      console.debug("loading dependency", scriptSrc);
+  for (const src of srcs) {
+    if (LOAD_STATUS[src]) {
+      throw new Error(`Attempt to load ${src} twice!`);
+    }
+    LOAD_STATUS[src] = {
+      done: 0,
+      total: 0,
+    };
+  }
+  const xhrs: { [src in Src]: XMLHttpRequest } = srcs.
+    map((assetSrc: Src) => {
+      console.debug("loading dependency", assetSrc);
 
       const xhr = new XMLHttpRequest;
 
-      xhr.open('GET', scriptSrc, true);
+      xhr.open('GET', assetSrc, true);
       xhr.onload = function (e) {
-        if (xhr.readyState === 2 && !LOAD_STATUS[scriptSrc]) {
-          LOAD_STATUS[scriptSrc] = {
-            done: 0,
-            total: parseInt(
-              xhr.getResponseHeader('content-length') || '0',
-              10),
-          };
+        if (xhr.readyState === 2) {
+          LOAD_STATUS[assetSrc].total = parseInt(
+            xhr.getResponseHeader('content-length') || '0',
+            10);
         }
         if (xhr.readyState === 4) {
           if (xhr.status === 200) {
@@ -50,7 +56,11 @@ function loadScript<SRCS extends readonly string[]>(
         handleError((e.currentTarget as XMLHttpRequest).status);
       };
       xhr.onprogress = function (e) {
-        LOAD_STATUS[scriptSrc] = { done: e.loaded, total: e.total };
+        if (!LOAD_STATUS[assetSrc]) {
+          throw new Error(`Unknown asset ${assetSrc} (not being loaded)`);
+        }
+        LOAD_STATUS[assetSrc].done = e.loaded;
+        LOAD_STATUS[assetSrc].total = e.total;
         onProgress(
           Object.values(LOAD_STATUS).map(e => e['done']),
           Object.values(LOAD_STATUS).map(e => e['total']),
@@ -60,21 +70,21 @@ function loadScript<SRCS extends readonly string[]>(
       xhr.send();
 
       function handleError(status: number, e?: unknown) {
-        console.error("failed to load dependency", scriptSrc, status, xhr.response, e);
+        console.error("failed to load dependency", assetSrc, status, xhr.response, e);
         if (status === 404) {
-          onError(`${scriptSrc} was not found.`);
+          onError(assetSrc, "URL was not found");
         } else if (status === 500) {
-          onError(`Server error occurred while serving ${scriptSrc}.`);
+          onError(assetSrc, "Server error occurred while serving");
         } else if (status === 0) {
-          onError(`Request was aborted or content length incorrect for ${scriptSrc}.`);
+          onError(assetSrc, "Request was aborted or content length incorrect");
         } else {
-          onError(`Server returned an unexpected HTTP status for ${scriptSrc}.`, xhr.response);
+          onError(assetSrc, "Server returned an unexpected HTTP status", xhr.response);
         }
 
         abortAll();
       }
 
-      return { [scriptSrc]: xhr } as { [key in SRCS[number]]: XMLHttpRequest };
+      return { [assetSrc]: xhr } as { [key in Src]: XMLHttpRequest };
     }).
     reduce((prev, curr) => ({ ...prev, ...curr }));
 
@@ -87,7 +97,7 @@ function loadScript<SRCS extends readonly string[]>(
 
     if (!unfinished) {
       for (const scriptSrc of Object.keys(xhrs)) {
-        onDone(scriptSrc);
+        onDone(scriptSrc as Src);
       }
     }
   }
@@ -101,32 +111,49 @@ function loadScript<SRCS extends readonly string[]>(
   return abortAll;
 }
 
-const progressEl = document.createElement('progress');
-progressEl.style.marginBottom = '1em';
-const statusEl = document.createElement('p');
 const app = document.getElementById('app')!;
 app.innerHTML = '';
-app.appendChild(progressEl);
-app.appendChild(statusEl);
 
-loadScript(
-  ['./index.js', './index.css'],
-  (done_, total_) => {
-    const done = done_.reduce((a, v) => a + v);
-    const total = total_.reduce((a, v) => a + v);
-    if (done < total) {
-      progressEl.setAttribute('max', String(total));
-      progressEl.setAttribute('value', String(done));
-    } else {
-      progressEl.removeAttribute('max');
-      progressEl.removeAttribute('value');
+function createProgress(assetSrc: string): { bar: HTMLElement, label: HTMLElement } {
+  if (!assets[assetSrc]) {
+    const progressEl = document.createElement('progress');
+    progressEl.style.marginBottom = '1em';
+    const statusEl = document.createElement('p');
+    app.appendChild(progressEl);
+    app.appendChild(statusEl);
+    assets[assetSrc] = { bar: progressEl, label: statusEl };
+  }
+  return assets[assetSrc];
+}
+const assets:
+Record<string, { bar: HTMLElement, label: HTMLElement }> =
+{} as const;
+
+const assetSrcs = ['./index.js', './index.css'] as const;
+loadAsset<typeof assetSrcs[number]>(
+  assetSrcs,
+  function handleProgress (done_, total_) {
+    for (const [idx, ] of total_.entries()) {
+      const assetSrc = assetSrcs[idx];
+      const { bar: progressEl, label: statusEl } = createProgress(assetSrc);
+      const done = done_[idx];
+      const total = total_[idx];
+      console.debug(idx, assetSrc, done, total);
+      if (done < total) {
+        progressEl.setAttribute('max', String(total));
+        progressEl.setAttribute('value', String(done));
+      } else {
+        progressEl.removeAttribute('max');
+        progressEl.removeAttribute('value');
+      }
+      statusEl.innerHTML = `Loading ${assetSrc}<br/>${byteFormatter.format(done)}`;
     }
-    statusEl.innerHTML = `Loading Paneron Web<br/>${byteFormatter.format(done)}`;
   },
-  (err) => {
-    statusEl.innerHTML = `An error occurred during the initial loading sequence.<br />${err}`;
+  function handleError (assetSrc, err) {
+    const { label: statusEl } = createProgress(assetSrc);
+    statusEl.innerHTML = `An error occurred fetching ${assetSrc}.<br />${err}.`;
   },
-  (src) => {
+  function handleDone (src) {
     if (src.endsWith('.js')) {
       const tag = document.createElement('script');
       tag.setAttribute('src', src);
