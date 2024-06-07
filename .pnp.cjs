6bb7766e7701fdc67dc451e36779cde8bce70bcd
Anton <anton@strogonoff.name>
2024-06-07 22:59:57 +0800

    chore(deps/spa): bump RegistryKit

--- .pnp.cjs
+++ .pnp.cjs
@@ -345,7 +345,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
       ],\
       [\
         "@riboseinc/paneron-registry-kit",\
-        "npm:2.2.48"\
+        "npm:2.2.49"\
       ],\
       [\
         "@types/dom4",\
@@ -2726,10 +2726,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["@riboseinc/paneron-registry-kit", [\
-        ["npm:2.2.48", {\
-          "packageLocation": "./.yarn/cache/@riboseinc-paneron-registry-kit-npm-2.2.48-e172cc55cb-35c595479d.zip/node_modules/@riboseinc/paneron-registry-kit/",\
+        ["npm:2.2.49", {\
+          "packageLocation": "./.yarn/cache/@riboseinc-paneron-registry-kit-npm-2.2.49-9253398672-dba6effea1.zip/node_modules/@riboseinc/paneron-registry-kit/",\
           "packageDependencies": [\
-            ["@riboseinc/paneron-registry-kit", "npm:2.2.48"]\
+            ["@riboseinc/paneron-registry-kit", "npm:2.2.49"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -5210,7 +5210,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@emotion/react", "virtual:3dd5dd780e0edb3d713803220c302ff49b2ef41811bb055852197ae4f488e15dbd9bde26d60fe23c3a284318e1057ed25ae8e86a80fc97b6e96884646c004ca3#npm:11.10.6"],\
             ["@emotion/styled", "virtual:3dd5dd780e0edb3d713803220c302ff49b2ef41811bb055852197ae4f488e15dbd9bde26d60fe23c3a284318e1057ed25ae8e86a80fc97b6e96884646c004ca3#npm:11.10.6"],\
             ["@riboseinc/paneron-extension-kit", "virtual:3dd5dd780e0edb3d713803220c302ff49b2ef41811bb055852197ae4f488e15dbd9bde26d60fe23c3a284318e1057ed25ae8e86a80fc97b6e96884646c004ca3#npm:2.2.29"],\
-            ["@riboseinc/paneron-registry-kit", "npm:2.2.48"],\
+            ["@riboseinc/paneron-registry-kit", "npm:2.2.49"],\
             ["@types/node", "npm:20.11.0"],\
             ["@types/react", "npm:18.2.46"],\
             ["@types/react-dom", "npm:18.2.18"],\
