4c5df7803fa4e5135a1dc617c213a5565eb242c0
Anton <anton@strogonoff.name>
2024-09-04 16:35:12 +0800

    chore(spa/deps): bump RegistryKit

--- .pnp.cjs
+++ .pnp.cjs
@@ -353,7 +353,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
       ],\
       [\
         "@riboseinc/paneron-registry-kit",\
-        "npm:2.2.55"\
+        "npm:2.2.56"\
       ],\
       [\
         "@types/dom4",\
@@ -2863,10 +2863,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["@riboseinc/paneron-registry-kit", [\
-        ["npm:2.2.55", {\
-          "packageLocation": "./.yarn/cache/@riboseinc-paneron-registry-kit-npm-2.2.55-932b9ac4fe-c17253c91f.zip/node_modules/@riboseinc/paneron-registry-kit/",\
+        ["npm:2.2.56", {\
+          "packageLocation": "./.yarn/cache/@riboseinc-paneron-registry-kit-npm-2.2.56-759d43c27d-4491605724.zip/node_modules/@riboseinc/paneron-registry-kit/",\
           "packageDependencies": [\
-            ["@riboseinc/paneron-registry-kit", "npm:2.2.55"]\
+            ["@riboseinc/paneron-registry-kit", "npm:2.2.56"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -5551,7 +5551,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@emotion/styled", "virtual:3dd5dd780e0edb3d713803220c302ff49b2ef41811bb055852197ae4f488e15dbd9bde26d60fe23c3a284318e1057ed25ae8e86a80fc97b6e96884646c004ca3#npm:11.10.6"],\
             ["@jsonurl/jsonurl", "npm:1.1.7"],\
             ["@riboseinc/paneron-extension-kit", "virtual:3dd5dd780e0edb3d713803220c302ff49b2ef41811bb055852197ae4f488e15dbd9bde26d60fe23c3a284318e1057ed25ae8e86a80fc97b6e96884646c004ca3#npm:2.2.29"],\
-            ["@riboseinc/paneron-registry-kit", "npm:2.2.55"],\
+            ["@riboseinc/paneron-registry-kit", "npm:2.2.56"],\
             ["@types/node", "npm:20.11.0"],\
             ["@types/react", "npm:18.2.46"],\
             ["@types/react-dom", "npm:18.2.18"],\
